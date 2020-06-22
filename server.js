ebbddf9a425ea8aac667463870abd0c08a6a2acf
tehlordvortex <tehlordvortex@outlook.com>
2020-06-23 00:41:13 +0100

    setup express server and heroku postbuild

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + '/dist'));
+// Have vue-router handle routing instead
+app.use(function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);
+console.log('server started ' + port);
